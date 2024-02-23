21cda5d8a95f24f84854dd72b9cf74ab7ebebd07
unknown <ngordon@mail.ru>
2024-02-23 20:10:42 +0200

    untitled2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int ticket = 17600;
+        int bonus = 20;
+        int result = ticket / bonus;
+        System.out.println(result);
+
     }
 }
